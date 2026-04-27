c32ced9ae7608bd4be660fbe89bf2215b6dbb002
ADNAN-ZEYA <adnanzeya5@gmail.com>
2026-04-28 00:39:07 +0530

    test pipeline

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CI/CD Pipeline Working 🚀');
+  res.send('Version 2 - Green Deployment 🚀');
 });
 
 app.listen(3000, () => console.log('Server running'));
